f4c6b1571f278f46d1383e8db45ef15d6196203a
antony_sanders <antony.devon92@gmail.com>
2020-05-08 03:19:47 -0400

    1.Updated belongs to user relationship to be optional. 2.changed the price validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-    belongs_to :user #, optional: true
+    belongs_to :user , optional: true
     belongs_to :store, optional: true
     belongs_to :shopping_list
 
     validates :name, presence: true
-    validates :price,  length: { :minimum => 0.01 }
+    validates :price, inclusion: { :in => 0.1..399.99 }
 end
